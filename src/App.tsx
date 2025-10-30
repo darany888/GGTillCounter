8380ea2f2ce29b994b080845e1a2f23711575587
darany888 <sengdarany@gmail.com>
2025-10-30 20:00:34 +1300

    Minor header name change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import TillCounter from './components/TillCounter';
 function App(): JSX.Element {
   return (
     <div id="App" className="App">
-      <Header title="Glou Glou Cashup" />
+      <Header title="Glou Glou Cash Up" />
       <TillCounter />
     </div>
   );
